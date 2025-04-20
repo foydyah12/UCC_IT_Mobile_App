d23c8c1bb87ccebea51a2c2137e6d5e1ce5b0343
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 21:23:32 -0600

    Update TwitterFragment.kt

--- app/src/main/java/com/example/uccitmobileapp/ui/home/TwitterFragment.kt
+++ app/src/main/java/com/example/uccitmobileapp/ui/home/TwitterFragment.kt
@@ -7,9 +7,11 @@ import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.uccitmobileapp.R
 
+//This method is to ensure proper fragment creation 
 class TwitterFragment : Fragment(R.layout.fragment_twitter) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val webView: WebView = view.findViewById(R.id.twitterWebView)
+        //To handle webpage within the app instead of browser, for a better web functionality using Javascript and to load UCC Twitter webpage
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("https://twitter.com/uccjamaica")
