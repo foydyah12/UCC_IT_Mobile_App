f47fdfd81bf40a7d36e4478eedfe0027a1cfa1e4
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 20:34:08 -0600

    Update FacultyModel.kt

--- app/src/main/java/com/example/uccitmobileapp/FacultyModel.kt
+++ app/src/main/java/com/example/uccitmobileapp/FacultyModel.kt
@@ -1,5 +1,7 @@
+// Define the package for the application
 package com.example.uccitmobileapp
 
+//Data class to represent a faculty member's details
 data class FacultyModel(
     val name: String,
     val email: String,
