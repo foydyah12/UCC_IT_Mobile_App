43cd7c713c5368939fcc5ddc2dd22882912612e5
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 21:01:12 -0600

    Update SocialMediaPagerAdapter.kt

--- app/src/main/java/com/example/uccitmobileapp/SocialMediaPagerAdapter.kt
+++ app/src/main/java/com/example/uccitmobileapp/SocialMediaPagerAdapter.kt
@@ -7,10 +7,12 @@ import com.example.uccitmobileapp.ui.home.InstagramFragment
 import com.example.uccitmobileapp.ui.home.TikTokFragment
 import com.example.uccitmobileapp.ui.home.TwitterFragment
 
+//Helps manage fragments in a ViewPager2 component, handling different social media pages
 class SocialMediaPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = 4
 
+    //Creates and returns the appropriate fragment based on the position in the pager
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> InstagramFragment()
