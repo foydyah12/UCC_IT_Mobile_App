4cdb9234f5286280015ecb2293af2a8133cfce5e
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 21:20:58 -0600

    Update TikTokFragment.kt

--- app/src/main/java/com/example/uccitmobileapp/ui/home/TikTokFragment.kt
+++ app/src/main/java/com/example/uccitmobileapp/ui/home/TikTokFragment.kt
@@ -7,9 +7,11 @@ import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.uccitmobileapp.R
 
+//This method is to ensure proper fragment creation
 class TikTokFragment : Fragment(R.layout.fragment_tiktok) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val webView: WebView = view.findViewById(R.id.tiktokWebView)
+        //To handle loading of the webpage within the app, improve web functionality using Javascript and to load the UCC webpages
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("https://www.tiktok.com/@uccjamaica")
