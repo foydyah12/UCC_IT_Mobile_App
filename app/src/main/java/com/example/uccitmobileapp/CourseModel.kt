3d7d741c31ff72a29288d152b661c5113a4894ef
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 19:42:57 -0600

    Update CourseModel.kt

--- app/src/main/java/com/example/uccitmobileapp/CourseModel.kt
+++ app/src/main/java/com/example/uccitmobileapp/CourseModel.kt
@@ -1,5 +1,7 @@
+// Define the package for the application
 package com.example.uccitmobileapp
 
+// Data class to represent a course with its attributes
 data class CourseModel(
     val code: String,
     val name: String,
