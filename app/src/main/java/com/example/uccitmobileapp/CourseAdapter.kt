a192f5fb0e595cde04bb8c572a92c368c0f0f996
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 19:38:01 -0600

    Update CourseAdapter.kt

--- app/src/main/java/com/example/uccitmobileapp/CourseAdapter.kt
+++ app/src/main/java/com/example/uccitmobileapp/CourseAdapter.kt
@@ -6,9 +6,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+//Adapter class for the RecyclerView that displays a list of courses
 class CourseAdapter(private val courseList: List<CourseModel>) :
     RecyclerView.Adapter<CourseAdapter.CourseViewHolder>() {
 
+        //ViewHolder class to hold and bind views for a single item
     class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val code: TextView = itemView.findViewById(R.id.txtCourseCode)
         val name: TextView = itemView.findViewById(R.id.txtCourseName)
@@ -16,13 +18,15 @@ class CourseAdapter(private val courseList: List<CourseModel>) :
         val prerequisites: TextView = itemView.findViewById(R.id.txtCoursePrerequisites)
         val description: TextView = itemView.findViewById(R.id.txtCourseDescription)
     }
-
+    
+// Inflates the layout for each item in the RecyclerView and creates ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_course, parent, false)
         return CourseViewHolder(view)
     }
 
+    // Binds data from the course list to the views in the ViewHolder
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
         val course = courseList[position]
         holder.code.text = course.code
