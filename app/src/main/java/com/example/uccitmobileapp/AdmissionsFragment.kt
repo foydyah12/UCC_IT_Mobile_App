e8dd7daad7229c9b1183e50118dc7852e57a69b0
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 19:33:30 -0600

    Update AdmissionsFragment.kt

--- app/src/main/java/com/example/uccitmobileapp/AdmissionsFragment.kt
+++ app/src/main/java/com/example/uccitmobileapp/AdmissionsFragment.kt
@@ -12,22 +12,29 @@ import androidx.fragment.app.Fragment
 
 // this defines the admissionsfragment
 class AdmissionsFragment : Fragment() {
+    
+    //Suppress lint warning for missing ID
     @SuppressLint("MissingInflatedId")
+
+    //This defines the Fragment's UI
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_admission, container, false)
-
+        
+ //TextView element with ID 'applicationLink' and assign it
         val applyLink = view.findViewById<TextView>(R.id.applicationLink)
         val applyLinks = view.findViewById<TextView>(R.id.internationalLink)
 
+        // Set an OnClickListener on 'applyLink' to open the UCC website
         applyLink.setOnClickListener {
             val url = "https://ucc.edu.jm/"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
 
+         // Set an OnClickListener on 'applyLinks' to open the international students requirements page
         applyLinks.setOnClickListener {
             val url = "https://ucc.edu.jm/programmes/undergraduate/requirements-for-international-students"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
