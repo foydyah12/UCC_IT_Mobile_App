9d4b8e3437a92edb2522ea4a7c2b84e726d32566
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 21:11:20 -0600

    Update InstagramFragment.kt

--- app/src/main/java/com/example/uccitmobileapp/ui/home/InstagramFragment.kt
+++ app/src/main/java/com/example/uccitmobileapp/ui/home/InstagramFragment.kt
@@ -7,9 +7,11 @@ import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.uccitmobileapp.R
 
+//This fragment represnts an Instagram page and loads it inside a WebView
 class InstagramFragment : Fragment(R.layout.fragment_instagram) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val webView: WebView = view.findViewById(R.id.instagramWebView)
+        //Sets a WebViewClient to handle the loading of the web pages within the app, JavaScript for better web functionality and to load the UCC webpage
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("https://www.instagram.com/uccjamaica/")
