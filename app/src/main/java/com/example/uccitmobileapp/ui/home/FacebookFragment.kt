f82b8cd9b2c4ca1ce9d9560bdc0d3a4bd9818f7a
Tarik573 <twilson35@stu.ucc.edu.jm>
2025-04-19 21:06:22 -0600

    Update FacebookFragment.kt

--- app/src/main/java/com/example/uccitmobileapp/ui/home/FacebookFragment.kt
+++ app/src/main/java/com/example/uccitmobileapp/ui/home/FacebookFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.uccitmobileapp.R
 
+//THis fragment represents a Facebook page and automatically oprns it when the fragment is created
 class FacebookFragment : Fragment() {
 
     override fun onCreateView(
@@ -17,10 +18,12 @@ class FacebookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //Opens the Facebook page as soon as the fragment's view is created
         openFacebookPage()
         return inflater.inflate(R.layout.fragment_facebook, container, false)
     }
 
+    //This function launches an intent to open the specified Facebook page in a browser or app
     private fun openFacebookPage() {
         val facebookUrl = "https://www.facebook.com/uccjamaica"
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl))
